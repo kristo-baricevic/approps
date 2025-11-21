804829cc327562c3d3bae168ea3f3d136fb33960
Kristo Baricevic <kristo.baricevic@gmail.com>
2025-11-20 21:00:41 -0500

    update

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -235,8 +235,34 @@ export default function Page() {
               bills.
             </p>
           </div>
-
-          <div className="flex items-center space-x-3 rounded-full border border-slate-700 bg-slate-900/70 px-3 py-2 shadow-sm shadow-black/40">
+          <div className="">
+            <Link
+              href="https://github.com/kristo-baricevic/approps"
+              target="_blank"
+              className="inline-flex items-center rounded-lg border border-slate-600 bg-slate-900/70 px-3 py-2 text-sm text-slate-200 hover:bg-slate-700 transition mb-4"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-5 h-5 mr-2"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12 2C6.477 2 2 6.486 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483
+      0-.237-.009-.868-.014-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608
+      1.004.07 1.532 1.033 1.532 1.033.892 1.53 2.341 1.087 2.91.832.092-.647.35-1.087.636-1.337-2.22-.255-4.555-1.113-4.555-4.951
+      0-1.093.39-1.988 1.029-2.688-.103-.255-.446-1.285.098-2.677 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0 1 12 6.844c.85.004
+      1.705.115 2.504.337 1.909-1.296 2.748-1.026 2.748-1.026.546 1.392.203 2.422.1 2.677.64.7 1.028 1.595 1.028 2.688
+      0 3.848-2.339 4.692-4.566 4.942.359.31.678.92.678 1.855 0 1.338-.012 2.419-.012 2.749
+      0 .268.18.58.687.482A10.019 10.019 0 0 0 22 12.017C22 6.486 17.522 2 12 2z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              GitHub Repository
+            </Link>
+          </div>
+          <div className="flex items-center space-x-3 rounded-full border border-slate-700 bg-slate-900/70 px-3 py-2 shadow-sm shadow-black/40 mb-4">
             <span className="text-xs uppercase tracking-wide text-slate-400">
               API health
             </span>
